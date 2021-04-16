9e674d8207a8dbd5e4455c43e201cabd28a78b5c
Murilo Campaner <murilo@inovad.com.br>
2021-04-15 23:20:00 -0300

    feat(rn): added react-native (asyncStorage) support

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import { cache } from 'swr';
 
-function getStorage(mode: 'local' | 'session') {
+const STORAGE_KEY_PREFIX = 'swr-';
+
+let asyncStorage: any;
+try {
+  asyncStorage = require('@react-native-async-storage/async-storage');
+} catch (error) {}
+
+function getStorage(mode: 'local' | 'session' | 'asyncStorage') {
   switch (mode) {
     case 'local':
       return localStorage;
     case 'session':
       return sessionStorage;
+    case 'asyncStorage': {
+      if (asyncStorage === undefined) {
+        throw new Error('AsyncStorage is not available. Check if "@react-native-async-storage/async-storage" is installed as a dependency in package.json')
+      }
+      return asyncStorage;
+    }
     default: {
       throw new Error(
         `Invalid mode ${mode}, it must be either local or session.`
@@ -21,15 +34,33 @@ function baseParser(value: string): any {
   return value === 'undefined' ? undefined : JSON.parse(value);
 }
 
-export function syncWithStorage(
-  mode: 'local' | 'session',
+function getAsyncStorageKeyPairs(storage: any): any {
+  const keys = storage.getAllKeys();
+  const swrKeys = keys.filter((key: string) => key.startsWith(STORAGE_KEY_PREFIX));
+  const callback = new Promise(async (resolve, reject) => {
+    try {
+      storage.multiGet(swrKeys, resolve);
+    } catch (error) {
+      reject(error);
+    }
+  })
+
+  return callback;
+}
+
+export async function syncWithStorage(
+  mode: 'local' | 'session' | 'asyncStorage',
   parser = baseParser
 ) {
+  
   const storage = getStorage(mode);
+  const storageKeyPairs = mode === 'asyncStorage'
+    ? await getAsyncStorageKeyPairs(storage)
+    : Object.entries(storage);
 
   // Get all key from the storage
-  for (let [key, data] of Object.entries(storage)) {
-    if (!key.startsWith('swr-')) continue;
+  for (let [key, data] of storageKeyPairs) {
+    if (!key.startsWith(STORAGE_KEY_PREFIX)) continue;
     // update SWR cache with the value from the storage
     cache.set(
       key.slice(4),
@@ -58,4 +89,8 @@ export function syncWithLocalStorage(parser?: typeof baseParser) {
 
 export function syncWithSessionStorage(parser?: typeof baseParser) {
   return syncWithStorage('session', parser);
+}
+
+export function syncWithAsyncStorage(parser?: typeof baseParser) {
+  return syncWithStorage('asyncStorage', parser);
 }
